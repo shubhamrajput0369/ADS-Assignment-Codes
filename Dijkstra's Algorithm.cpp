d8508c90ae1db4f36b41ac710a8b022af8ba21e6
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-04-02 13:34:49 +0530

    Update Dijkstra's Algorithm.cpp

--- Dijkstra's Algorithm.cpp
+++ Dijkstra's Algorithm.cpp
@@ -17,3 +17,13 @@ public:
 		int cost,i,j;
 		cout<<"\n Enter number of vertex ";
 		cin>>V;
+		for(i=1;i<=V;++i)
+			for (j=1;j<=V;++j)
+				G[i][j]=INF;
+		cout<<"\n Enter number of edges ";
+		cin>>E;
+		for(i=0;i<E;++i)
+		{
+			int m,n;
+			cout<<"\n Enter start and end vertex and its cost :";
+			cin>>m>>n>>cost;
