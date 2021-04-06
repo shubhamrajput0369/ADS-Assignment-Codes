c846a91bb79578d820ffca3906534d448ace6976
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-04-06 22:59:34 +0530

    Update Inheritance.cpp

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -16,3 +16,33 @@ class Personal
     {
         cout<<"\nEnter the name of student ";
         cin>>name;
+
+        cout<<"\nEnter the age ";
+        cin>>age;
+    }
+    void putData()
+    {
+        cout<<"\nName of Student :- "<<name;
+        cout<<"\nAge of Student :- "<<age;
+    }
+};
+class Academic
+{
+    private:
+    int roll;
+    float per;
+
+    public:
+    void getData()
+    {
+        cout<<"\nEnter the roll number of student ";
+        cin>>roll;
+        cout<<"\nEnter the marks of the student ";
+        cin>>per;
+    }
+    void putData()
+    {
+        cout<<"\nRoll Number :- "<<roll;
+        cout<<"\nMarks of Student :- "<<per;
+    }
+};
