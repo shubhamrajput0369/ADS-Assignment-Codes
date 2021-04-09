f83892ab75cb41eda2cdbc44b33635559a3ff664
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-04-09 20:38:57 +0530

    Update TemplateAdd.cpp

--- TemplateAdd.cpp
+++ TemplateAdd.cpp
@@ -17,3 +17,6 @@ int main()
     int a=7, b=7;
     float x=5.5, y=7.7;
     long g=1234567, h=12348;
+    double c=5.555555555, d=7.7777777777;
+
+    cout<<"*****ADDITION USING TEMPLATE FUNCTION*****\n";
